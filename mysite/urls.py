2e031bfcd3bbd81b776face22aea3ebf55824788
lindseyyii <lyi46@gatech.edu>
2024-09-24 15:49:25 -0400

    add forgot password url

--- mysite/urls.py
+++ mysite/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('signup/', views.signup, name='signup'),
     path('login/', views.login, name='login'),
+    path('forgotpassword/', views.forgot_password, name='forgot_password'),
 ]
