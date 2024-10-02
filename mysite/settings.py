3c3e4773153ea65e4f5107fc181648ee1d5df85f
lindseyyii <lyi46@gatech.edu>
2024-10-02 11:45:27 -0400

    Revert "delete"
    This reverts commit 1c5f97eba0645c7df3dab24c6d515b61ebdfd89c, reversing
    changes made to 4b632365ec0bbadb2afbc58bfad6eec577208c7a.

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
-import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -56,7 +55,7 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'restaurants', 'templates')],
+        'DIRS': [BASE_DIR / 'restaurants/templates', '/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -71,8 +70,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'mysite.wsgi.application'
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
@@ -101,10 +98,6 @@ AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
-]
-
-AUTHENTICATION_BACKENDS = [
-    'django.contrib.auth.backends.ModelBackend',
 ]
 
 
--- restaurants/apps.py
+++ restaurants/apps.py
@@ -1,8 +1,6 @@
 from django.apps import AppConfig
 
+
 class RestaurantsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'restaurants'
-
-    def ready(self):
-        import restaurants.signals  # Ensure your signals are imported
